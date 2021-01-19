a4f20765697b02ab8d011178f40215fd12131d6c
harshvishah2 <sofinehs2@gmail.com>
2021-01-20 00:58:07 +0530

    added an arc

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
+var g = 100;
+var b = 255;
+
 function setup() {
-    createCanvas(windowWidth,windowHeight);
-  }
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0, g, b);
   
-  function draw() {
-    background(0,200,255);
-    rect(mouseX, mouseY, 100,100);
-  }
+  g = map(mouseX, 0, 600, 0, 255);
+  b = map(mouseX, 600, 0, 255, 0);
+
+  fill(100, 0, 250)
+  stroke(0, 255, 255);
+  // quad(30,10,50,10, 69, 63, 30, 76);
+
+  arc(mouseX, mouseY, 500, 500, 10, 100);
+  fill(0, 210, 255);
+  arc(100, 100, 100, 100, 100, 100, 100, 100)
+}
