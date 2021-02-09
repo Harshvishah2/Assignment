5f0668dcdf3bbb2d2d64d75d0018556ccfd647aa
harshvishah2 <sofinehs2@gmail.com>
2021-02-10 00:11:52 +0530

    First try

--- sketch.js
+++ sketch.js
@@ -1,21 +1,44 @@
-let g = 100;
-let b = 255;
+let xoff=0.0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  alert("This is the statement for the piece titled: Project1. It is an effect that looks like a butterfly. It comprises of classic blue colour lines on loop. There are ellipses behind the lines which create an illusion of stars.They sort of twinkle as you move your cursor.Hold on your cursor for 3-4 seconds at one place to see the effect happens. The background has a dissapearing effect to it.")
 }
 
 function draw() {
-  background(0, g, b);
+  background("#ffffff05");
 
-  g = map(mouseX, 0, 600, 0, 255);
-  b = map(mouseX, 600, 0, 255, 0);
 
-  fill(100, 0, 250)
-  stroke(0, 255, 255);
-  // quad(30,10,50,10, 69, 63, 30, 76);
+  let x = 0;
+  let y = 0;
 
-  arc(mouseX, mouseY, 500, 500, 10, 100);
-  fill(0, 210, 255);
-  arc(100, 100, 100, 100, 100, 100, 100, 100)
+  for (let x = 0; x <= width; x = x + 50) {
+
+    for (let y = 0; y <=height; y = y + 50) {
+
+      //fill(random(200), 150, random(170));
+      fill(255);
+      noStroke();
+      ellipse(x, y,4,4);
+    }
+  }
+  
+  
+  for (let x = 0; x <= width; x = x + 10) {
+
+    for (let y = 0; y <=height; y = y + 10) {
+
+    
+  //   stroke( 0, 0, random(151));
+      
+    stroke(0, 62, 81);
+     line(x,y,mouseX,mouseY);
+      
+      
+   // line(x,y,mouseX,mouseY);
+      
+    }
+  }
+ 
 }
